ef2e36086aa6b768b29a43e8218e2bc55187f79e
unknown <guilherme-yoshida@hotmail.com>
2025-11-08 16:28:37 -0300

    second commit

---
+++ src/main/java/com/example/demo/entities/Autor.java
@@ -0,0 +1,13 @@
+package com.example.demo.entities;
+
+import java.time.Instant;
+
+public class Autor {
+
+    private Long id;
+    private String nome;
+    private String nacionalidade;
+    private Instant dataNascimento;
+    private String biografia;
+
+}
